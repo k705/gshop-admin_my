69832008d0ad6675b658098e5257af6f0cd711f7
study73 <464043271@qq.com>
2023-07-10 22:54:00 +0800

    添加框完成

--- src/api/trademark.ts
+++ src/api/trademark.ts
@@ -11,10 +11,23 @@ export interface ResTrademark{
 export type ResTrademarkByPage = ResPage<ResTrademark[]>;
 
 enum URLS{
-    LIST = "/product/baseTrademark"
+    LIST = "/product/baseTrademark",
+    ADD = "/product/baseTrademark/save"
 }
 
 // 请求品牌列表数据
 export function requestTrademarkListByPage(page =1,limit=5) {
         return request.get<any,ResTrademarkByPage>(`${URLS.LIST}/${page}/${limit}`)
     }
+
+
+/* -----------------------------添加页---------- */
+    // 定义数据类型
+export interface ReqSaveBaseTrademark {
+    tmName: string,
+    logoUrl:string
+    }
+    // 添加品牌请求
+export function requestSaveBaseTrademark(data:ReqSaveBaseTrademark) {
+    return request.post<any,null>(URLS.ADD,data)
+}
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,51 +1,75 @@
 <template>
-  <el-card shadow="hover">
-    <!-- 头部为按钮组件 插槽-->
-    <template #header>
-      <el-button type="primary" icon="ele-plus"> 添加 </el-button>
-    </template>
+  <div>
+    <el-card shadow="hover">
+      <!-- 头部为按钮组件 插槽-->
+      <template #header>
+        <el-button
+          type="primary"
+          icon="ele-Plus"
+          @click="showAddTrademarkDialog"
+        >
+          添加
+        </el-button>
+      </template>
 
-    <!-- footer-table组件 -->
-    <el-table :data="trademarks">
-      <el-table-column label="序号" type="index"></el-table-column>
-      <el-table-column label="品牌名称" prop="tmName"></el-table-column>
-      <el-table-column label="品牌LOGO">
-        <template #="{ row }">
-          <img :src="row.logoUrl" width="100" />
-        </template>
-      </el-table-column>
-      <el-table-column label="操作"></el-table-column>
-    </el-table>
-    <!-- footer-pagination组件 -->
-    <el-pagination
-      :total="total"
-      :current-page="page"
-      :page-size="pageSize"
-      :page-sizes="[5, 10, 15]"
-      layout="prev, pager, next,jumper,->,sizes,total"
-      @current-change="handleCurrentChange"
-      @size-change="handleSizeChange"
-    >
-    </el-pagination>
-  </el-card>
+      <!-- footer-table组件 -->
+      <el-table :data="trademarks">
+        <el-table-column label="序号" type="index"></el-table-column>
+        <el-table-column label="品牌名称" ></el-table-column>
+        <el-table-column label="品牌LOGO" >
+          <template #="{ row }">
+            <img :src="row.logoUrl" width="100" />
+          </template>
+        </el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+      <!-- footer-pagination组件 -->
+      <el-pagination
+        :total="total"
+        :current-page="page"
+        :page-size="pageSize"
+        :page-sizes="[5, 10, 15]"
+        layout="prev, pager, next,jumper,->,sizes,total"
+        @current-change="handleCurrentChange"
+        @size-change="handleSizeChange"
+      >
+      </el-pagination>
+    </el-card>
 
- <!--  <el-dialog title="添加品牌" v-model="isShowAddTrademarkDialog" >
-  <el-form :model="form">
-    <el-form-item label="品牌名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" autocomplete="off"></el-input>
-    </el-form-item>
-    <el-form-item label="品牌Logo" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-  <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    <el-dialog title="添加品牌" v-model="isShowAddTrademarkDialog">
+      <el-form
+        label-width="100px"
+        :model="formData"
+        :rules="rules"
+        ref="formRef"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input v-model="formData.tmName" />
+        </el-form-item>
+        <el-form-item label="品牌Logo" prop="logoUrl">
+          <el-upload
+            class="avatar-uploader"
+            :action="uploadUrl"
+            :show-file-list="false"
+            :on-success="onSuccessHandler"
+          >
+            <img
+              v-if="formData.logoUrl"
+              :src="formData.logoUrl"
+              class="avatar"
+            />
+            <el-icon v-else class="avatar-uploader-icon">
+              <ele-Plus />
+            </el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="notShowAddTrademarkDialog">取 消</el-button>
+        <el-button type="primary" @click="addTrademarkHandler">确 定</el-button>
+      </template>
+    </el-dialog>
   </div>
-</el-dialog> -->
 </template>
 
 <script lang="ts">
@@ -61,16 +85,56 @@ export default defineComponent({
 import { ref } from "vue";
 
 // 导入请求
-import { requestTrademarkListByPage } from "@/api/trademark";
+import {
+  requestTrademarkListByPage,
+  requestSaveBaseTrademark,
+} from "@/api/trademark";
 
 // 导入类型
-import type { ResTrademark } from "@/api/trademark";
+import type { ResTrademark, ReqSaveBaseTrademark } from "@/api/trademark";
+import type { FormRules } from "element-plus";
+import { ElMessage } from "element-plus";
 
 // 定义数据类型
 const trademarks = ref<ResTrademark[]>([]);
 const total = ref(0);
 const page = ref(1);
 const pageSize = ref(5);
+const formRef = ref();
+const uploadUrl = `${import.meta.env.VITE_API_URL}/product/upload`;
+const rules: FormRules = {
+  tmName: [
+    {
+      required: true,
+      min: 1,
+      max: 20,
+      message: "品牌名称必须是1-20个字符之间",
+    },
+  ],
+  logoUrl: [
+    {
+      required: true,
+      validator(rules: any, value: string, callback) {
+        if (value === "") {
+          callback("品牌名必填");
+        } else {
+          const ext = value.substring(value.lastIndexOf(".") + 1);
+          if (!["png", "gif", "jpg"].includes(ext)) {
+            callback("品牌LOGO必须是图片");
+          } else {
+            callback();
+          }
+        }
+      },
+    },
+  ],
+};
+
+const isShowAddTrademarkDialog = ref(false);
+const formData = ref<ReqSaveBaseTrademark>({
+  tmName: "",
+  logoUrl: "",
+});
 
 // 声明方法
 // 请求trademark
@@ -95,8 +159,77 @@ function handleSizeChange(v: number) {
   pageSize.value = v;
   getTrademarkListByPage();
 }
-// 调用方法
+
+/* ---------------------添加------------------------------ */
+// 点击添加显示添加框
+function showAddTrademarkDialog() {
+  isShowAddTrademarkDialog.value = true;
+}
+function notShowAddTrademarkDialog() {
+  isShowAddTrademarkDialog.value = false;
+}
+
+// 点击添加框的确定按钮发送请求
+async function addTrademarkHandler() {
+  try {
+    console.log("formRef", formRef.value);
+
+    // 调用 formRef.value.validate() 方法来对这个表单中指定的数据（model、rules）进行校验
+  
+      const res = await formRef.value.validate();
+      console.log("校验成功", res);
+ 
+
+    await requestSaveBaseTrademark(formData.value);
+    // 隐藏
+    isShowAddTrademarkDialog.value = false;
+    // 提示
+    ElMessage.success("添加成功");
+    // 获取最新的列表数据
+    // 把当前页定到最后一页（新增的数据就在最后一页）
+    page.value = Math.ceil((total.value + 1) / pageSize.value);
+    getTrademarkListByPage();
+  } catch (e) {
+    ElMessage.error("添加失败");
+  }
+}
+
+// 文件上传成功时的钩子
+function onSuccessHandler(response: { code: number; data: string }) {
+  if (response.code === 200) {
+    formData.value.logoUrl = response.data;
+  }
+}
+
+// 调用获取列表数据方法
 getTrademarkListByPage();
 </script>
 
-<style scoped></style>
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
